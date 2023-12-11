f16bace8043ffbe7baa2c1a4946757811213509a
Alfffos <alfonsogaray03@gmail.com>
2023-12-11 20:36:35 -0300

    Last Comments

--- Agenda_api/Models/DTOs/CreateAndUpdateContact.cs
+++ Agenda_api/Models/DTOs/CreateAndUpdateContact.cs
@@ -12,8 +12,6 @@ namespace Agenda_api.Models.DTOs
         public bool favorite { get; set; }
 
         public string Description = String.Empty;
-        public User? User;
-
-                
+        public User? User;         
     }
 }
--- Agenda_api/Program.cs
+++ Agenda_api/Program.cs
@@ -1,67 +1,4 @@
-﻿//using Agenda_api.Data;
-//using Agenda_api.Models;
-//using Agenda_api.Repository;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.OpenApi.Models;
-//using Microsoft.IdentityModel.Tokens;
-//using Microsoft.OpenApi.Models;
-//using System.Text;
-//using AutoMapper;
-
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-
-//builder.Services.AddControllers();
-//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen(setupAction =>
-//{
-//    setupAction.AddSecurityDefinition("Agenda_apiBearerAuth", new OpenApiSecurityScheme()
-//    {
-//        Type = SecuritySchemeType.Http,
-//        Scheme = "Bearer",
-//        Description = "Aca pegar el toke generado la logearse."
-//    });
-//    setupAction.AddSecurityRequirement(new OpenApiSecurityRequirement 
-//    { 
-//        { new OpenApiSecurityScheme 
-//             { 
-//                Reference = new OpenApiReference 
-//                { 
-//                Type= ReferenceType.SecurityScheme,Id="AgendaApiBearerAuth"} },new List<string>() }}
-//    );
-//});
-
-// #region
-// builder.Services.AddSingleton<UserRepository>();               //ESTO NO SE PARA QUE SE USA PERO ESTABA EN EL NOTION DEL PROFE... MAS ADELANTE SEGURAMENTE VEA PARA QUE SIRVE. IMPORTANTE VER LO DE SINGELTON CREO QUE ES DEL TOKEN.
-// builder.Services.AddSingleton<ContactRepository>();           // Esto es para que el ContactRepository y el UserRepository puedan ser usados como servicios y yo pueda llamar a los metodos (Para que yo los pueda inyectar) en este caso como Singelton.
-// #endregion
-
-////Add Context
-//builder.Services.AddDbContext<AgendaApiContext>(dbContextOptions => dbContextOptions.UseSqlite(
-//    builder.Configuration["ConnectionStrings:AgendaAPIDBConnectionString"]));   //Esta es la coneccion a la base de datos (esto trabaja con el appsettings.json.
-
-//var app = builder.Build();
-
-//builder.Services.AddAuthentication("Bearer")
-//    .AddJwtBearer()
-
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-//app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-
-//app.MapControllers();
-
-//app.Run();
+﻿
 
 using Agenda_api.Controllers;
 using Agenda_api.Repository;
@@ -154,7 +91,7 @@ if (app.Environment.IsDevelopment())
 }
 
 
-app.UseCors("AllowWebapp");
+app.UseCors("AllowWebapp   ");
 
 app.UseHttpsRedirection();
 
